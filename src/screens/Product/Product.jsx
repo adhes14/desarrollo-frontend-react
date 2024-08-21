b4d03997d23ada8f79534a5b17ea2281a7c655c0
adhes14 <adhemarduran@gmail.com>
2024-08-20 22:05:02 -0400

    fix warning in useEffect

--- src/screens/Product/Product.jsx
+++ src/screens/Product/Product.jsx
@@ -10,17 +10,17 @@ const Product = () => {
   useEffect(() => {
     dispatch(
       setProducts([
-        {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-        {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-        {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-        {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-        {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-        {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
       ])
     );
-  }, []);
+  }, [dispatch]);
   return (
-    <div style={{width: '300px', margin: '0 auto'}}>
+    <div style={{ width: '300px', margin: '0 auto' }}>
       <h1>Fruits</h1>
       <FilterableProductTable products={products} />
     </div>
