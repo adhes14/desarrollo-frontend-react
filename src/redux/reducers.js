3d3bc7934c660764b130ab79b1fddece9279eca1
adhes14 <adhemarduran@gmail.com>
2024-08-21 21:23:23 -0400

    add forms to redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 // Import custom reducers
 import defaultReducer from './default/defaultReducer';
 import productsReducer from './products/productsReducers';
+import formReducer from './form/formReducers';
 
 const rootReducer = combineReducers({
   default: defaultReducer,
   products: productsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
--- src/screens/Forms/LoginForm.jsx
+++ src/screens/Forms/LoginForm.jsx
@@ -1,14 +1,23 @@
 import useForm from "../../hooks/useForm";
+import { useSelector, useDispatch } from "react-redux";
+import { saveFormData } from "../../redux/form/formActions";
 
 const LoginForm = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
+
+  const form = useSelector(state => state.form);
+  const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
+    dispatch(saveFormData(values));
   }
   return (
     <form onSubmit={handleSubmit}>
       <h1>Login</h1>
+      <h2>email: { form.formData.email }</h2>
+      <h2>password: { form.formData.password }</h2>
       <div>
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" value={values.email} onChange={handleChange} />
