2eea7464d095c42d7217aeb55d3f192f3e3e0e39
adhes14 <adhemarduran@gmail.com>
2024-08-21 21:03:33 -0400

    add form

--- src/screens/Forms/LoginForm.jsx
+++ src/screens/Forms/LoginForm.jsx
@@ -1,15 +1,22 @@
+import useForm from "../../hooks/useForm";
+
 const LoginForm = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Login</h1>
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password
-        <input type="password" name="password" />
-      </label>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" value={values.email} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" value={values.password} onChange={handleChange} />
+      </div>
       <button type="submit">Login</button>
     </form>
   );
