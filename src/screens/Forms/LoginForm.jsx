aa683bf0b40c5b64ff05a3a2ef629aa277a17401
adhes14 <adhemarduran@gmail.com>
2024-08-22 20:28:37 -0400

    add form styles

--- src/screens/Forms/LoginForm.jsx
+++ src/screens/Forms/LoginForm.jsx
@@ -14,20 +14,24 @@ const LoginForm = () => {
     dispatch(saveFormData(values));
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <h2>email: { form.formData.email }</h2>
-      <h2>password: { form.formData.password }</h2>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" value={values.email} onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" value={values.password} onChange={handleChange} />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <h2>email: { form.formData.email }</h2>
+        <h2>password: { form.formData.password }</h2>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" value={values.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" value={values.password} onChange={handleChange} />
+        </div>
+        <div className="button-container">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
